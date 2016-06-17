2405d9b093e32acc6fcf2df721737b1784b6c307
Matt McVicar <matthewmcvicar@gmail.com>
2016-06-16 22:03:13 -0700

    second

--- aggregate_health/views.py
+++ aggregate_health/views.py
@@ -4,7 +4,7 @@ from .models import Patient
 # Create your views here.
 def patient_list(request):
 	patients = Patient.objects.all().values('firstName', 'lastName', 'date_of_birth', 'primarycareprovider__firstName', 'primarycareprovider__lastName')
-	return render(request, 'aggregate_health/patient_search_results.html', {'patients': patients})
+	return render(request, 'aggregate_health/patient_search_results.html', {'patients': patients, 'nbar': 'search_results'})
 
 def dashboard(request):
-	return render(request, 'aggregate_health/dashboard.html')
+	return render(request, 'aggregate_health/dashboard.html', {'nbar':'dash'})
